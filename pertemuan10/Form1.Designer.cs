b3574fd53c5889ef1c68db65aba19b73b1a1cb81
IvanFebriansyah <ivanfebriansyah@protonmail.com>
2022-01-03 01:25:46 +0700

    Add project files.

---
+++ pertemuan10/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace pertemuan10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtuser = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtpwd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvuser = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvuser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User Id";
+            // 
+            // txtuser
+            // 
+            this.txtuser.Location = new System.Drawing.Point(105, 29);
+            this.txtuser.Name = "txtuser";
+            this.txtuser.Size = new System.Drawing.Size(318, 20);
+            this.txtuser.TabIndex = 1;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtpwd
+            // 
+            this.txtpwd.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtpwd.Font = new System.Drawing.Font("Wingdings", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.txtpwd.Location = new System.Drawing.Point(105, 65);
+            this.txtpwd.Name = "txtpwd";
+            this.txtpwd.Size = new System.Drawing.Size(318, 20);
+            this.txtpwd.TabIndex = 4;
+            this.txtpwd.TextChanged += new System.EventHandler(this.txtpwd_TextChanged);
+            this.txtpwd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtpwd_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Password";
+            // 
+            // dgvuser
+            // 
+            this.dgvuser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvuser.Location = new System.Drawing.Point(16, 170);
+            this.dgvuser.Name = "dgvuser";
+            this.dgvuser.Size = new System.Drawing.Size(438, 293);
+            this.dgvuser.TabIndex = 8;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Image = global::pertemuan10.Properties.Resources.imageres_94;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(274, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 45);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Delete";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Image = global::pertemuan10.Properties.Resources.shell32_270;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.Location = new System.Drawing.Point(173, 108);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 45);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Update";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = global::pertemuan10.Properties.Resources.imageres_56;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(85, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 45);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Save";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 475);
+            this.Controls.Add(this.dgvuser);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtpwd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtuser);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Data Pengguna";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvuser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtuser;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtpwd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dgvuser;
+    }
+}
+
---
+++ pertemuan10/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace pertemuan10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-ABD7H3A;Initial Catalog=kantinAmikom;Integrated Security=True");
+
+        private void resetdata()
+        {
+            txtuser.Text = "";
+            txtpwd.Text = "";
+        }
+
+        private void showdata()
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM admin";
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(ds, "admin");
+            dgvuser.DataSource = ds;
+            dgvuser.DataMember = "admin";
+            dgvuser.ReadOnly = true;
+        }
+
+        private string CaesarCipher(string value, int shift)
+        {
+            string[] joinCipher = new string[200];
+            string joinText = "";
+            string[] wordArray = value.Split(' ');
+
+            try
+            {
+                for(int x = 0; x < wordArray.Length; x++)
+                {
+                    char[] buffer = wordArray[x].ToCharArray();
+
+                    for(int i = 0; i < buffer.Length; i++)
+                    {
+                        char letter = buffer[i];
+                        letter = (char)(letter + shift);
+
+                        if(letter > 'z')
+                        {
+                            letter = (char)(letter - 26);
+                        }else if(letter < 'a')
+                        {
+                            letter = (char)(letter + 26);
+                        }
+
+                        buffer[i] = letter;
+                    }
+                    string HasilKonversi = new string(buffer);
+                    joinCipher[x] = HasilKonversi;
+                }
+
+                joinText = string.Join(" ", joinCipher);
+                return joinText;
+
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return null;
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(txtuser.Text == "" | txtpwd.Text == "")
+            {
+                MessageBox.Show("Semua data harus diisi", "Warning");
+                goto berhenti;
+            }
+
+            string tekscipher = null;
+            tekscipher = CaesarCipher(txtpwd.Text, 17);
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "INSERT INTO admin values ('" + txtuser.Text + "','" + tekscipher + "')";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            showdata();
+            resetdata();
+
+        berhenti:
+            ;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resetdata();
+            showdata();
+        }
+
+        private void txtpwd_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void txtpwd_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.KeyChar = Convert.ToChar(e.KeyChar.ToString().ToLower());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if(txtuser.Text == "")
+            {
+                MessageBox.Show("Isi user id yang akan dihapus");
+                goto berhenti;
+            }
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "DELETE FROM admin WHERE userid = '" + txtuser.Text + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            showdata();
+            resetdata();
+
+        berhenti:
+            ;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(txtuser.Text == "" | txtpwd.Text == "")
+            {
+                MessageBox.Show("Semua data harus diisi", "Peringatan");
+                goto berhenti;
+            }
+
+            string tekscipher = null;
+            tekscipher = CaesarCipher(txtpwd.Text, 17);
+
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "UPDATE admin set password= '" + tekscipher + "'WHERE userid='" + txtuser.Text + "'";
+            cmd.ExecuteNonQuery();
+            con.Close();
+            showdata();
+            resetdata();
+
+        berhenti:
+            ;
+        }
+    }
+}
